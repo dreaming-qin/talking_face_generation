bbee5cae49bcd8eaf8a8253e36cf384835f74caa
dreaming-qin <18697951462@163.com>
2024-03-11 09:51:27 +0800

    更新新的audio encode在test_my.py中

--- test_my.py
+++ test_my.py
@@ -12,6 +12,7 @@ import torchvision
 import shutil
 from PIL import Image
 from random import sample
+from torch import nn
 
 
 
@@ -27,6 +28,9 @@ from scipy.io import loadmat
 from src.model.syncNet.sync_net import SyncNet
 from src.util.util_3dmm import get_lm_by_3dmm,get_face
 from Deep3DFaceRecon_pytorch.util.util import draw_landmarks
+from src.util.model_util import cnt_params
+from src.model.exp3DMM.self_attention_pooling import SelfAttentionPooling
+from src.model.exp3DMM.fusion import PositionalEncoding
 
 
 
@@ -68,20 +72,67 @@ from Deep3DFaceRecon_pytorch.util.util import draw_landmarks
 
 
 if __name__=='__main__':
-    # 将256*256的视频与原始音频合并
-    video_list=glob.glob('/workspace/code/EAMM-main/data/emo/*.mp4')
-    video_list=sample(video_list, 15)
-    for video in video_list:
-        str_list=os.path.basename(video).split('_')
-        iden=str_list[0]
-        angle=str_list[1]
-        emo=str_list[2]
-        level=f'{str_list[3]}_{str_list[4]}'
-        mp4=str_list[5]
-        orginal_path=f'/workspace/dataset/MEAD/{iden}/video/{angle}/{emo}/{level}/{mp4}'
-        cmd ='ffmpeg -i {} -i {} -loglevel error -c copy -map 0:0 -map 1:1 -y -shortest {}'.format(
-            video,orginal_path,f'temp/{os.path.basename(video)}')
-        os.system(cmd) 
+    # # 定义编码器，词典大小为10，要把token编码成128维的向量
+    # embedding = nn.Embedding(10, 128)
+    # # 定义transformer，模型维度为128（也就是词向量的维度）
+    # transformer = nn.Transformer(d_model=128, batch_first=True) # batch_first一定不要忘记
+    # # 定义源句子，可以想想成是 <bos> 我 爱 吃 肉 和 菜 <eos> <pad> <pad>
+    # src = torch.LongTensor([[0, 3, 4, 5, 6, 7, 8, 1, 2, 2]])
+    # # 定义目标句子，可以想想是 <bos> I like eat meat and vegetables <eos> <pad>
+    # tgt = torch.LongTensor([[0, 3, 4, 5, 6, 7, 8, 1, 2]])
+    # # 将token编码后送给transformer（这里暂时不加Positional Encoding）
+    # outputs = transformer(embedding(src), embedding(tgt))
+    # outputs.size()
+
+
+    input=torch.ones((1,37,28,12))
+
+    # 初始化模型
+    num_encoder_layers=7
+    feature_dim=256
+
+    mapping_net=nn.Sequential(
+        nn.Linear(12,64),
+        nn.Linear(64,128),
+        nn.ReLU(True),
+        nn.Linear(128,256),
+    )
+
+    
+    encoder_layer = nn.TransformerEncoderLayer(d_model=feature_dim, nhead=8,
+                    dim_feedforward=512,batch_first=True)
+    encoder_norm = nn.LayerNorm(feature_dim)
+    encoder = nn.TransformerEncoder(encoder_layer, num_encoder_layers, encoder_norm)
+
+    self_atten=SelfAttentionPooling(input_dim=feature_dim)
+
+    # 音频长度28
+    pos=PositionalEncoding(feature_dim, n_position=28)
+
+    # test
+    temp=0
+    temp+=cnt_params(encoder)[0]
+    temp+=cnt_params(mapping_net)[0]
+    temp+=cnt_params(self_atten)[0]
+    temp+=cnt_params(pos)[0]
+    print(temp)
+
+    # 前向传播
+    out=mapping_net(input)
+
+    B,L,_,dim=out.shape
+    out=out.reshape(B*L,-1,dim)
+    pos_embd=pos(out.shape[1])
+    # pytorch中的transformer未进行pos操作，在这进行操作
+    out=out+pos_embd
+    out=encoder(out)
+
+    out=self_atten(out)
+    out=out.reshape(B,L,dim)
+    print(out.shape)
+
+    
+
 
 
 
